0029d4ffa90d3b1b04e8281e65ee3c698de19cf7
Avoka99 <benjaminlahadi99@gmail.com>
2023-11-28 08:44:33 -0800

    Implementing if else statements

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 
-formatted_alphabet = ''.join([chr(i) for i in range(97, 123) if chr(i) not in ['e', 'q']])
+formatted_alphabet = ''.join(
+    [chr(i) for i in range(97, 123) if chr(i) not in ['e', 'q']]
+)
 print("{}".format(formatted_alphabet), end='')
