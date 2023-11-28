c2b13324a8f84bd0a59eb847411630491600df2d
Avoka99 <benjaminlahadi99@gmail.com>
2023-11-28 08:36:19 -0800

    Implementing if else statements

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 
-for i in range(97, 123):
-    print(chr(i), end='')
+formatted_alphabet = ''.join([chr(i) for i in range(97, 123)])
+print("{}".format(formatted_alphabet), end='')
