99b79851ca2041ccdf13f9a62e1116f16ebfaa06
Avoka99 <benjaminlahadi99@gmail.com>
2023-12-01 08:35:43 -0800

    Import modules and functions

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,7 +4,7 @@ from sys import argv
 if __name__ == "__main__":
     num_args = len(argv) - 1
     args_str = "arguments" if num_args != 1 else "argument"
-    
+
     print("{:d} {:s}{:s}".format(num_args, args_str, ":" if num_args else "."))
 
     for i in range(1, num_args + 1):
