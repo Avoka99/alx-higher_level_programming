7edcd877964ebd2267a23366e24a32a6902cba0d
Avoka99 <benjaminlahadi99@gmail.com>
2023-12-07 18:27:17 -0800

    Python data structures

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,53 +1,40 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+
 /**
- * is_palindrome - Checks if a singly linked list is a palindrome.
- * @head: Pointer to the head of the list
- *
- * Return: 1 if it is a palindrome, 0 otherwise.
+ * is_palindrome - checks if a singly linked list is a palindrome
+ * @head: pointer to head of list
+ * Return: 1 if it is a palindrome, 0 otherwise
  */
 int is_palindrome(listint_t **head)
 {
-	listint_t *slow = *head;
-	listint_t *fast = *head;
-	listint_t *prev_slow = *head;
-	listint_t *second_half;
-	listint_t *prev = NULL;
-	listint_t *current = second_half;
-	listint_t *next_node;
-	listint_t *first_half;
+	listint_t *slow = *head, *fast = *head, *prev = NULL, *tmp;
 
-	if (*head == NULL || (*head)->next == NULL)
+	if (!head || !*head)
 		return (1);
-	while (fast != NULL && fast->next != NULL)
+
+	while (fast && fast->next)
 	{
 		fast = fast->next->next;
-		prev_slow = slow;
+
+		tmp = slow->next;
+		slow->next = prev;
+		prev = slow;
+		slow = tmp;
+	}
+
+	if (fast)
+		slow = slow->next;
+
+	while (prev && slow)
+	{
+		if (prev->n != slow->n)
+			return (0);
+
+		prev = prev->next;
 		slow = slow->next;
 	}
-	if (fast == NULL)
-	{
-		second_half = slow;
-	}
-	else
-	{
-		second_half = slow->next;
-	}
-	while (current != NULL)
-	{
-		next_node = current->next;
-		current->next = prev;
-		prev = current;
-		current = next_node;
-	}
-	second_half = prev;
-	first_half = *head;
-	while (second_half != NULL)
-	{
-		if (first_half->n != second_half->n)
-		{
-			return (0);
-		}
-		first_half = first_half->next;
-		second_half = second_half->next;
-	}
+
 	return (1);
 }
