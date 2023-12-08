761b928bfed25819bd71df5ffdbf79085dd7020f
Avoka99 <benjaminlahadi99@gmail.com>
2023-12-07 18:11:23 -0800

    Python data structures

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    filtered_string = ''.join(char for char in my_string if char.lower() not in {'c', 'C'})
+    filtered_string = ''
+    for char in my_string:
+        if char.lower() not in {'c', 'C'}:
+            filtered_string += char
     return filtered_string
